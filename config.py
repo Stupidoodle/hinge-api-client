1d89520ebff04adc03ac1a80df3520df1d8f62a7
Bryan <bryan.tran@travania.app>
2025-07-19 22:48:20 +0200

    feat: enhance configuration with dynamic Sendbird URLs and additional app versioning

--- config.py
+++ config.py
@@ -8,7 +8,11 @@ class Settings(BaseSettings):
 
     # --- Constants & Configuration ---
     # You didn't think I'd hardcode this shit, did you?
-    BASE_URL = "https://prod-api.hingeaws.net"
-    SENDBIRD_API_URL = "https://api-3cdad91c-1e0d-4a0d-bbee-9671988bf9e9.sendbird.com"
-    SENDBIRD_WS_URL = "wss://ws-3cdad91c-1e0d-4a0d-bbee-9671988bf9e9.sendbird.com"
-    SENDBIRD_APP_ID = "3CDAD91C-1E0D-4A0D-BBEE-9671988BF9E9"
+    BASE_URL: str = "https://prod-api.hingeaws.net"
+    SENDBIRD_APP_ID: str = "3CDAD91C-1E0D-4A0D-BBEE-9671988BF9E9"
+    SENDBIRD_API_URL: str = f"https://api-{SENDBIRD_APP_ID.lower()}.sendbird.com"
+    SENDBIRD_WS_URL: str = f"wss://ws-{SENDBIRD_APP_ID.lower()}.sendbird.com"
+    HINGE_APP_VERSION: str = "9.82.0"
+    HINGE_BUILD_NUMBER: str = "11616"
+    OS_VERSION: str = "26.0"
+    
